6acedf857cf09c55e5e2d298fd776b4760abf84c
manjudhiman <manjudhiman@users.noreply.github.com>
2024-09-10 15:41:16 +0530

    bottomUp approach

--- LongestCommonSubsequence.py
+++ LongestCommonSubsequence.py
@@ -24,8 +24,33 @@ class Solution:
             return dp[i][j]
         
         return findLongestSubsequence(text1, text2,0,0, dp)
+
+
+
+#Bottom Up Time and space complexity O(m*n)
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        rows = len(text1)
+        cols = len(text2)
+        dp = [[0 for _ in range(cols+1)] for _ in range(rows+1)]
+        
+        
+        for i in range(1, rows+1):
+            for j in range(1, cols+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    
+        return dp[rows][cols]
+        
+        
+    
         
         
         
         
         
+    
+  
