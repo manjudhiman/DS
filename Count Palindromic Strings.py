3055b0ae1f817f13ed7727f09d7160109ed608a7
manjudhiman <manjudhiman@users.noreply.github.com>
2024-10-03 08:40:18 +0530

    Create Count Palindromic Strings.py

---
+++ Count Palindromic Strings.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        
+        length = len(s)
+        
+        dp = [[0 for _ in range(length)] for _ in range(length)]
+        
+        count = 0
+        for i in range(length):
+            dp[i][i] = 1
+            count += 1
+            
+            
+        for start in reversed(range(length)):
+            for end in range(start+1, length):
+                if s[start] == s[end]:
+                    pLength = end-start+1
+
+                    if pLength == 2 or pLength-2 == dp[start+1][end-1]:
+                        dp[start][end] = pLength
+                        count += 1
+
+                    else:
+                        dp[start][end] = max(dp[start+1][end], dp[start][end-1])
+                else:
+                    dp[start][end] = max(dp[start+1][end], dp[start][end-1])
+                    
+        return count
+
+            
+            
+        
